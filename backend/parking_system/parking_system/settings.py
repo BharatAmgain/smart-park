774a7473dccde2354c082f5b185d6e1096a79793
BharatAmgain <176702267+BharatAmgain@users.noreply.github.com>
2026-05-04 20:06:05 +0545

    Initial project structure

--- backend/parking_system/parking_system/settings.py
+++ backend/parking_system/parking_system/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'amgaibharat46@gmail.com'
-EMAIL_HOST_PASSWORD = 'qyqb pfde frfm oznj'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'amgaibharat46@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 OTP_EXPIRY_SECONDS = 300
 
 # ========== eSewa ==========
